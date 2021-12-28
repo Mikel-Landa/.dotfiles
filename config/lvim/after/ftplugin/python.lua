d7291eb6754a45c0aa9e7e8a03ae69ef5c1846ae
Mikel Landa <mikel923@hotmail.com>
2021-12-28 10:42:33 +0100

    👌 IMPROVE:

--- config/lvim/after/ftplugin/python.lua
+++ config/lvim/after/ftplugin/python.lua
@@ -1,11 +1,1 @@
-local formatters = require "lvim.lsp.null-ls.formatters"
-formatters.setup({
-    {exe = "black", filetypes = {"python"} },
-    {exe = "isort", filetypes = {"python"} },
-
-  })
-
-local linters = require "lvim.lsp.null-ls.linters"
-linters.setup {
-  { exe = "flake8" ,filetypes = {"python"}},
-}
+/home/mikel/.dotfiles/config/lvim/after/ftplugin/python.lua
